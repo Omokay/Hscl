d862b0827fad17baad7e1be191a4c8f46b3415f4
Omokay <chuku.omoke@gmail.com>
2019-07-24 23:25:25 +0100

    save latest

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 $(document).ready(function () {
     var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:4,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:1000,
-    autoplayHoverPause:true
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
+    owl.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: false
+    });
 });
